3dd4915b93912eada45fd73f4669e67338ce4542
unknown <lelo80@gmail.com>
2023-01-25 13:51:17 -0300

    Entrega Final

--- Install.bat
+++ Install.bat
@@ -1,8 +1,29 @@
-echo "1 - Install Vagrant"
+@ECHO OFF
+@echo "--------------------------------------------------------------"
+echo "1 - Install Vagrant and Git"
+PAUSE
+@echo "--------------------------------------------------------------"
 echo "2 - Installing vagrant plugins... Please wait..."
-vagrant plugin install vagrant-azure
-vagrant plugin install vagrant-ssh-config
-vagrant box add azure https://github.com/msopentech/vagrant-azure/raw/master/dummy.box
+REM vagrant plugin install vagrant-azure
+REM vagrant plugin install vagrant-ssh-config
+REM vagrant plugin install vagrant-scp
+REM vagrant plugin install vagrant-host-shell
+REM vagrant box add azure https://github.com/msopentech/vagrant-azure/raw/master/dummy.box
+@echo "--------------------------------------------------------------"
 echo "git clone https://github.com/Lelandi/devops-challenge-20230110"
-echo "Change personal connection info (VagrantFile and SSH keys)"
-vagrant up control slave --provider=azure --no-parallel --provision
+@echo "--------------------------------------------------------------"
+@echo " "
+@echo "--------------------------------------------------------------"
+echo Change Azure access information on Vagrantfile (location: ansible-lab/control-node folder)
+@echo "--------------------------------------------------------------"
+@echo "--------------------------------------------------------------"
+@echo "--------------------------------------------------------------"
+PAUSE
+echo off
+REM %cd%
+echo on
+cd ansible-lab\control-node\
+echo on
+@echo "--------------------------------------------------------------"
+@echo "4 - Let me guide you through the process. Please Stand By...:"
+vagrant up --provider=azure --provision --color
--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -1,20 +1,16 @@
 #/bin/sh
-## Criar Chaves SSH
-yes '' | ssh-keygen -N '' > /dev/null
-## Fim de Criar Chaves SSH
-
-cp /root/.ssh/id_rsa.pub /vagrant/chave
-
+cp /vagrant/.ssh/id_rsa /home/vagrant/.ssh/
 sudo yum -y install epel-release
 echo "-----------------------------"
-echo "Instaling Ansible. Hold On..."
+echo "Installing Ansible. Hold On..."
 echo "-----------------------------"
 sudo yum -y install ansible
 ansible-galaxy collection install community.general
 echo "-----------------------------"
 echo "Modifying Hosts with VM Ip..."
 echo "-----------------------------"
-cat <<EOT >> /etc/hosts
+sudo -u vagrant cat <<EOT >> /etc/hosts
+127.0.0.1 control-node control-node
 10.0.0.5 control-node
 10.0.0.4 app01-coodesh
 EOT
@@ -22,7 +18,13 @@ cat <<EOT >> /etc/ansible/hosts
 [apps]
 10.0.0.4
 EOT
-echo "IaC With Ansible..."
-echo "IaC With Ansible..."
-echo "IaC With Ansible..."
-ansible-playbook /vagrant/playbooks/app.yml
+#Disabling SSH check
+sudo sed -i '/host_key_che/ s/#//' /etc/ansible/ansible.cfg
+sudo sed -i '/command_warnings/ s/# //' /etc/ansible/ansible.cfg
+echo "-----------------------------"
+echo "   PROVISION.SH COMPLETED    "
+echo "-----------------------------"
+sudo -u vagrant ssh-keyscan -H app01-coodesh >> /home/vagrant/.ssh/known_hosts
+sudo -u vagrant ssh -y app01-coodesh 
+sudo -u vagrant ansible-playbook /vagrant/playbooks/app.yml
+
